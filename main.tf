c2effe78133c1f4095c0233820586f3fe385ec7e
chandralekhasingasani <chandralekha.s@nutanix.com>
2023-07-08 12:08:10 +0530

    test

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ resource "aws_docdb_cluster" "docdb" {
   cluster_identifier      = "${var.COMPONENT}-${var.ENV}"
   engine                  = "${var.ENGINE}"
   engine_version          = var.ENGINE_VERSION
-  master_username         = jsondecode(data.aws_secretsmanager_secret_version.roboshop.secret_string)["RDS_USERNAME"]
-  master_password         = jsondecode(data.aws_secretsmanager_secret_version.roboshop.secret_string)["RDS_PASSWORD"]
+  master_username         = jsondecode(data.aws_secretsmanager_secret_version.roboshop.secret_string)["DOCUMENTDB_USERNAME"]
+  master_password         = jsondecode(data.aws_secretsmanager_secret_version.roboshop.secret_string)["DOCUMENTDB_PASSWORD"]
   skip_final_snapshot     = var.SKIP_FINAL_SNAPSHOT
   db_cluster_parameter_group_name = aws_docdb_cluster_parameter_group.example.name
   db_subnet_group_name = aws_docdb_subnet_group.default.name
