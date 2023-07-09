eef0d734d16a55d264bffd729889528731cc90d2
chandralekhasingasani <chandralekha.s@nutanix.com>
2023-07-09 21:13:21 +0530

    test

--- schema.tf
+++ schema.tf
@@ -6,8 +6,10 @@ cd /tmp
 unzip mongodb.zip
 cd mongodb-main
 wget https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem
+echo "downloaded"
 mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint}:27017 --sslCAFile global-bundle.pem --username ${jsondecode(data.aws_secretsmanager_secret_version.roboshop.secret_string)["DOCUMENTDB_USERNAME"]} --password ${jsondecode(data.aws_secretsmanager_secret_version.roboshop.secret_string)["DOCUMENTDB_PASSWORD"]}
 mongo < ${var.DB_NAME}.js
+
 EOF
   }
 }
